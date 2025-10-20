d0460c5a1189308c21f091e6b16407855804e4fa
Flydexo <flydexo@gmail.com>
2025-10-20 17:53:32 +0200

    fix floyd warshall paths

---
+++ bot2.py
@@ -0,0 +1,107 @@
+#! /usr/bin/python3
+from random import choice
+
+CASE_TYPE = 0
+CASE_RESS = 1
+CASE_SPEC = 2
+
+TYPE_MUR = 'W'
+TYPE_NORMAL = 'R'
+TYPE_DASH = 'D'
+TYPE_SHIELD = 'S'
+TYPE_FORCE = 'F'
+TYPE_MIDAS = 'M'
+TYPE_VITESSE = 'P'
+TYPE_MOI = 'I'
+TYPE_ENNEMI = 'E'
+
+MINION_PROP = 0
+MINION_X = 1
+MINION_Y = 2
+MINION_CARG = 3
+MINION_HP = 4
+MINION_CARG_MAX = 5
+MINION_ATTAQUE = 6
+
+with open("mapData.txt") as mapFile:
+    
+    n = int(mapFile.readline())
+
+    carte = [[None]*n for i in range(n)]
+    for i in range(n):
+        ligne = mapFile.readline()
+        for j,case in enumerate(ligne.strip().split()):
+            info = case.split(",")
+            carte[i][j] = [info[0], int(info[1]), int(info[2])]
+
+    m = int(mapFile.readline())
+    
+    minionsJoueurs = {}
+    for i in range(m):
+        *minion, = map(int, mapFile.readline().strip().split(","))
+        if minion[MINION_PROP] in minionsJoueurs:
+            minionsJoueurs[minion[MINION_PROP]].append(minion[1:])
+        else:
+            minionsJoueurs[minion[MINION_PROP]]=[minion[1:]]
+        
+    id, ressActuelles = map(int, mapFile.readline().strip().split())
+    depotX, depotY = map(int, mapFile.readline().strip().split())
+
+    if id in minionsJoueurs:
+        mesMinions = minionsJoueurs[id]
+    else:
+        mesMinions = []
+    for jid in minionsJoueurs:
+        for mX, mY, mCarg, mHp, mCargMax, mForce in minionsJoueurs[jid]:
+            carte[mX][mY][CASE_TYPE] = TYPE_MOI if jid == id else TYPE_ENNEMI
+
+    def enleve_impossibles(cands):
+        *cands, = filter(lambda c:c[0]>=0 and c[1]>=0 and c[0]<n and c[1]<n and carte[c[0]][c[1]][CASE_TYPE]!=TYPE_MUR, cands)
+        return cands
+
+    def choix_aleatoire(mX, mY, cands=None):
+        if cands!=None:
+            cands = enleve_impossibles(cands)
+        if cands==None or cands==[]:
+            cands = [(mX-1, mY), (mX+1, mY), (mX, mY-1), (mX, mY+1)]
+        cands = enleve_impossibles(cands)
+        if cands==[]:
+            return mX, mY
+        else:
+            return choice(cands)
+
+    with open("answer.txt", "w") as reponse:
+        mesMinions.sort(key = lambda m: abs(m[MINION_X]-depotX)+abs(m[MINION_Y]-depotY), reverse=True)
+        for mX, mY, mCarg, mHp, mCargMax, mForce in mesMinions:
+            if mCargMax > 0: # pas un soldat
+                if mCarg == mCargMax:
+                # Retour en direction du dépôt
+                    cands = []
+                    if mX < depotX:
+                        cands.append([mX+1, mY])
+                    elif mX > depotX:
+                        cands.append([mX-1, mY])
+                    if mY < depotY:
+                        cands.append([mX, mY+1])
+                    elif mY > depotY:
+                        cands.append([mX, mY-1])
+                    newX, newY = choix_aleatoire(mX, mY, cands)
+                    carte[mX][mY][CASE_TYPE] = TYPE_NORMAL
+                    carte[newX][newY][CASE_TYPE] = TYPE_MOI
+                    print(mX, mY, newX, newY, file=reponse)
+                else:
+                    if carte[mX][mY][CASE_RESS]>0:
+                        print(mX, mY, mX, mY, file=reponse) # Pomper
+                    else:
+                        cands = [(mX-1, mY), (mX+1, mY), (mX, mY-1), (mX, mY+1)]
+                        cands = enleve_impossibles(cands)
+                        choix = max(zip(map(lambda c:carte[c[0]][c[1]][CASE_RESS], cands), cands))
+                        if choix[0] == 0:
+                            newX, newY = choix_aleatoire(mX, mY)
+                        else:
+                            newX, newY = choix[1]
+                        carte[mX][mY][CASE_TYPE] = TYPE_NORMAL
+                        carte[newX][newY][CASE_TYPE] = TYPE_MOI
+                        print(mX, mY, newX, newY, file=reponse) # Se déplacer
+        if ressActuelles>4:
+            print("CREATE", 1, min(ressActuelles-1,10), 0, file=reponse)
--- template_ocaml.ml
+++ template_ocaml.ml
@@ -20,13 +20,13 @@ let minions = Array.init nMinions (fun i ->
 let myID, myResources = input_line ptr |> String.split_on_char ' ' |> List.map int_of_string |> (function | (a::b::[]) -> a,b | _ -> failwith "invalid 3") in
 let baseX, baseY = input_line ptr |> String.split_on_char ' ' |> List.map int_of_string |> (function | (a::b::[]) -> a,b | _ -> failwith "invalid 4") in
 let curTurn,maxTurn = input_line ptr |> String.split_on_char ' ' |> List.map int_of_string |> (function | (a::b::[]) -> a,b | _ -> failwith "invalid 5") in
-let randomEvents = input_line ptr
+(* let randomEvents = input_line ptr
   |> String.split_on_char ' '
   |> List.map (String.split_on_char ',')
   |> List.map (function |a::b::[] -> (int_of_string a, int_of_string b) | _ -> (-1, -1))
   |> List.filter (fun (a,b) -> a <> -1 || b <> -1)
   |> Array.of_list 
-in
+in *)
 
 close_in ptr;
 
@@ -42,10 +42,104 @@ TYPE_VITESSE = 'P'
 
 let ptrOut = open_out "answer.txt" in
 
-Array.iter (fun minion ->
-  if minion.owner = myID then begin
-    (* do things here *)
+let execute_instruction instruction fmt =
+  Printf.fprintf ptrOut instruction fmt;
+in
+
+let spawn_minion (hp:int) (size:int) (attack:int) =
+  if 2*hp + 2*size +attack > 25 then failwith "Stats exceed 25"
+  else execute_instruction "CREATE %d %d %d\n" hp size attack;
+in
+
+let move_minion (x: int) (y:int) (xd: int) (yd: int) =
+  execute_instruction "%d %d %d %d\n" x y xd yd
+in
+
+let spawn_initial_gechar () =
+  if curTurn = 0 then (spawn_minion 1 9 0)
+  (* else if curTurn = 1 then (move_minion baseX baseY (baseX+1) baseY; spawn_minion 1 9 0; )
+  else if curTurn = 2 then move_minion baseX baseY baseX (baseY+1) *)
+in
+
+if curTurn = 0 then begin
+  spawn_initial_gechar ();
+end;
+
+let extract (m:minion) = move_minion m.x m.y m.x m.y in
+
+let sign a = if a > 0 then 1 else -1 in
+
+let move_towards (m:minion) x y =
+  if x <> m.x then move_minion m.x m.y (m.x + sign (x - m.x)) m.y
+  else move_minion m.x m.y m.x (m.y + sign (y - m.y));
+in
+
+let floyd_warshall sx sy =
+  let lm = (Array.length map) in 
+  let distances = Array.make_matrix (lm*lm) (lm*lm) infinity in 
+  let paths = Array.make_matrix (lm*lm) (lm*lm) (-1,-1) in 
+  for j = 0 to lm*lm-1 do
+    let x = j/lm in
+    let y = j mod lm in
+    if y > 0 then (
+      let t,_,_ =  map.(x).(y-1) in
+      if t <> "W" then distances.(j).(x*lm+(y-1)) <- 1.;
+    );
+    if x > 0 then (
+      let t,_,_ =  map.(x-1).(y) in
+      if t <> "W" then distances.(j).((x-1)*lm+y) <- 1.;
+    );
+    if y < lm-1 then (
+      let t,_,_ =  map.(x).(y+1) in
+      if t <> "W" then distances.(j).(x*lm+(y+1)) <- 1.; 
+    );
+    if x < lm-1 then (
+      let t,_,_ =  map.(x+1).(y) in
+      if t <> "W" then distances.(j).((x+1)*lm+y) <- 1.; 
+    );
+    for k = 0 to lm*lm-1 do 
+      if distances.(lm*sx+sy).(k) +. distances.(k).(j) < distances.(lm*sx + sy).(j) then begin
+        distances.(lm*sx + sy).(j) <- distances.(lm*sx+sy).(k) +. distances.(k).(j);
+        paths.(lm*sx + sy).(j) <- if paths.(lm*sx + sy).(j) = (-1,-1) then (
+           Printf.printf "updated path with %d %d\n" (j/lm) (j mod lm);
+           (j / lm, j mod lm)
+        ) else paths.(lm*sx + sy).(j)
+      end
+    done
+  done; 
+  (distances,paths)
+in
+
+Array.iter (fun minion -> (
+  if minion.load = minion.capacity then 
+    move_towards minion baseX baseY
+  else begin
+    (* let distances = Array.init (Array.length map) (fun i -> Array.init (Array.length map.(0)) (fun j -> (abs (i - minion.x)) + (abs (j - minion.y)))) in *)
+    let distances, paths = floyd_warshall minion.x minion.y in
+    let gains = Array.init (Array.length map) (fun i -> Array.init (Array.length map.(0)) (fun j -> 
+      let t,r,_ = map.(i).(j) in
+      if t == "W" then 0
+      else min (minion.capacity - minion.load) r)) in 
+    let lm = Array.length map in
+    let ratio = Array.init (Array.length map) (fun i -> Array.init (Array.length map.(0)) (fun j -> (float_of_int (gains.(i).(j))) /. (distances.(minion.x*lm+minion.y).(i*lm+j)+.1.))) in  
+    let x = ref 0 in
+    let y = ref 0 in
+    let m = ref (-1.0 *. infinity) in
+    for i = 0 to lm-1 do
+      for j = 0 to lm-1 do
+        if ratio.(i).(j) > !m then begin
+          m := ratio.(i).(j);
+          x := i;
+          y := j;
+        end;
+      done; 
+    done;
+    let _,rd,_ = map.(!x).(!y) in
+    let next_x, next_y = paths.(lm*minion.x + minion.y).(!x*lm + !y) in
+    Printf.printf "x: %d y:%d px: %d py: %d\n" !x !y next_x next_y;
+    if !x = minion.x && !y = minion.y then extract minion
+    else move_towards minion next_x next_y
   end
-) minions;
+)) minions;
 
 close_out ptrOut
